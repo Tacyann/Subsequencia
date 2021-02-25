6ed3d7f524537075cadc6ee7800e2a459019073b
Tacyann <tacyblpimentel@gmail.com..br>
2021-02-24 21:39:53 -0300

    Renderizando

--- ME/src/App.java
+++ ME/src/App.java
@@ -1,4 +1,3 @@
-
 public class App {
     public static void imprimirSubsequencia( int vet[]){
         int maxSoma = Integer.MIN_VALUE;
@@ -15,19 +14,15 @@ public class App {
     }
 }
     System.out.print(maxSoma);
-}
-  
+} 
     public static void main(String[] args) throws Exception {
-        int [] vet = new int [9]; 
+        int [] vet = new int [6]; 
         vet[0]=-2;
-        vet[1]= 1;
-        vet[2]= -3;
-        vet[3]= 4;
-        vet[4]=-1;
+        vet[1]= 11;
+        vet[2]= -4;
+        vet[3]= 13;
+        vet[4]= -5;
         vet[5]= 2;
-        vet[6]= 1;
-        vet[7]= -5;
-        vet[8]= 4;
         imprimirSubsequencia(vet);        
 }
 }

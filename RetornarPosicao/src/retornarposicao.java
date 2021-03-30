3be180a1fc042ba2b15ef406436cf2282efca91a
Tacyann <tacyblpimentel@gmail.com..br>
2021-03-30 11:26:14 -0300

    ME04 - Retornar Posicao + Operacoes

--- RetornarPosicao/src/retornarposicao.java
+++ RetornarPosicao/src/retornarposicao.java
@@ -3,46 +3,45 @@ public class retornarposicao {
     
    //Metodo de Busca para retornar a posicao.
             private static int busca(int x, int[] vet) {
-                for ( int m = 0; m < vet.length; m++){
-                    if (vet[m] == x)//Se o meu vetor na posicao i for igual a x.
-                    return m;
+                for ( int m = 0; m < vet.length; m++){ //------------------------ 2 + 2n
+                    if (vet[m] == x)//Se o meu vetor na posicao i for igual a x. //---------------------------- n
+                    return m; //---------------------------- n
                 }
-               return 0;
+               return 0; //---------------------------- 1
 
     }
 
     public static void main(String[] args) {
-    Scanner ler = new Scanner(System.in);
-    int vetores[] = new int[3];
-    int i = 0;
+    Scanner ler = new Scanner(System.in);//---------------------------- 1
+    int vetores[] = new int[3]; //---------------------------- 1
+    int i = 0;//---------------------------- 1
         //Prencher o meu vetor
-        while( i < 3){
-        System.out.println("Digite o " + (i + 1) + "o numero: " );
-        vetores[i] =  ler.nextInt();
-        i++;
+        while( i < 3){ //------------------------n
+        System.out.println("Digite o " + (i + 1) + "o numero: " ); //---------------------------- 1
+        vetores[i] =  ler.nextInt(); //---------------------------- 1
+        i++; //------------------------n
         }
 
-        System.out.println("Digite o número que deseja buscar: ");
-        int x = ler.nextInt();
-        int aux = 0;
-        if( busca(x, vetores) != 0){
-            System.out.print("A Posição do valor procurado é: " + " " +busca(x, vetores));
+        System.out.println("Digite o número que deseja buscar: "); //---------------------------- 1
+        int x = ler.nextInt(); //---------------------------- 1
+        int aux = 0; //---------------------------- 1
+        if( busca(x, vetores) != 0){ //------------------------n
+            System.out.print("A Posição do valor procurado é: " + " " +busca(x, vetores)); //---------------------------- 1
         } else{
-            System.out.print("O numero procurado: " +busca(x, vetores)+ " não pertence ao Vetor!");
+            System.out.print("O numero procurado: " +busca(x, vetores)+ " não pertence ao Vetor!"); //---------------------------- 1
         }
         //ordenando o Vetor
-        for(int j = 0; j < vetores.length; j++){//pecorrer a linha
-            for(int k = 0; k < vetores.length; k++){//pecorrer a coluna
-                if(vetores[j]< vetores[k]){//vamos fazer uma troca
-                    aux = vetores[j];
-                    vetores[j] = vetores[k];
-                    vetores[k] = aux;
+        for(int j = 0; j < vetores.length; j++){//pecorrer a linha //------------------------ 2 + 2n
+            for(int k = 0; k < vetores.length; k++){//pecorrer a coluna //------------------------ 2 + 2n
+                if(vetores[j]< vetores[k]){//vamos fazer uma troca //------------------------n
+                    aux = vetores[j]; //------------------------n
+                    vetores[j] = vetores[k]; //------------------------n
+                    vetores[k] = aux; //------------------------n
                 }
 
             }
     }
-        
-   
-ler.close();
+          
+ler.close(); //---------------------------- 1
     }
 }
